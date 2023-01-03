0c3bff49243830e574476463f44e6640730abe1f
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-01-03 19:06:27 +0100

    messo tailwind invece di bs

--- partials/head.php
+++ partials/head.php
@@ -2,7 +2,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+  <!-- <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/> -->
+  <script src="https://cdn.tailwindcss.com"></script>
   <script src='https://cdnjs.cloudflare.com/ajax/libs/quagga/0.12.1/quagga.min.js' integrity='sha512-bCsBoYoW6zE0aja5xcIyoCDPfT27+cGr7AOCqelttLVRGay6EKGQbR6wm6SUcUGOMGXJpj+jrIpMS6i80+kZPw==' crossorigin='anonymous'></script>
   <link rel="stylesheet" href="styles/styles_general.css">
   <title>My Cassette Library</title>
